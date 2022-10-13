3269e93d4c314fdcda0bf47eb4297f2259001ba3
gLando23 <landen.gogel@evsck12.com>
2022-10-13 12:40:58 -0500

    10/13/22 * Commented python script. * Renamed CSV function to TSV.

--- tools/python/main.py
+++ tools/python/main.py
@@ -1,4 +1,7 @@
 from classes.window import WindowClass
 
+# Should be used to call startup classes.
+# If you are adding something major to the program
+# I recommend calling it here.
 if __name__ == "__main__":
     WindowClass()
